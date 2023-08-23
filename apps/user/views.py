92ca52a099e935532ef14518c021794f25be15b7
ortizfram <ortizfranco48@gmail.com>
2023-08-23 09:34:15 -0300

    CreateUserProfileView URL

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+# from apps.courses.models import CoursesLibrary, PaidCoursesLibrary
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.parsers import MultiPartParser, FormParser
+from .models import UserAccount
 
-# Create your views here.
+class CreateUserProfileView(APIView):
+    parser_classes = [MultiPartParser, FormParser]
+    def post(self, request, format=None):
+        data = self.request.data
+
+        account = data["account"]
+
+        user = UserAccount.objects.get_or_create(
+            account=account,
+            email='email',
+            username=account,
+            first_name='name',
+            last_name='last name',
+            location='location',
+            url='url',
+            birthday='2023-01-01',
+            profile_info='bio information',
+            age_limit='18+',
+            verified=False,
+            total_earnings=0,
+            total_spent=0,
+            sales=0,
+        )
+
+        # CoursesLibrary.objects.get_or_create(author=user[0])
+        # PaidCoursesLibrary.objects.get_or_create(author=user[0])
+
+        return Response({'success': 'Message sent successfully'})
